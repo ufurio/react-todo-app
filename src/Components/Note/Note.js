2db8fb5c3aed6b5975f03c58fa79f1350ce89e38
uryellll <ffurio.uriel@gmail.com>
2021-04-16 19:28:43 +0800

    change note color

--- src/Components/Note/Note.js
+++ src/Components/Note/Note.js
@@ -3,19 +3,19 @@ import "./Note.css";
 import { AiOutlineDelete, AiOutlineCheckSquare } from "react-icons/ai";
 
 function Note({ id, text, deleteNote, handleStatus, isCompleted }) {
-  return (
-    <div className="Note">
-      <h2 className={`text ${isCompleted ? "Completed" : ""}`}>{text}</h2>
-      <div className="EditRemoveBtn">
-        <button>
-          <AiOutlineCheckSquare onClick={() => handleStatus(id)} />
-        </button>
-        <button>
-          <AiOutlineDelete onClick={() => deleteNote(id)} />
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="Note">
+			<h2 className={`text ${isCompleted ? "Completed" : ""}`}>{text}</h2>
+			<div className="EditRemoveBtn">
+				<button onClick={() => handleStatus(id)}>
+					<AiOutlineCheckSquare />
+				</button>
+				<button onClick={() => deleteNote(id)}>
+					<AiOutlineDelete />
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Note;
